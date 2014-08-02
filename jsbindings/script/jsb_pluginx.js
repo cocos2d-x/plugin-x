903d4aae1b1f6b7fc877798fdd8a594a8279a80c
joshuastray <joshua.astray.14@gmail.com>
2014-08-02 14:52:11 +0800

    add facebook async methods

--- jsbindings/script/jsb_pluginx.js
+++ jsbindings/script/jsb_pluginx.js
@@ -57,3 +57,22 @@ plugin.FacebookAgent.HttpMethod = {};
 plugin.FacebookAgent.HttpMethod.Get = 0;
 plugin.FacebookAgent.HttpMethod.Post = 1;
 plugin.FacebookAgent.HttpMethod.Delete = 2;
+
+
+plugin.FacebookAgent.prototype._logout = plugin.FacebookAgent.prototype.logout;
+plugin.FacebookAgent.prototype.logout = function(callback){
+    this._logout();
+    callback(2, "logout success");
+};
+
+plugin.FacebookAgent.prototype._isLoggedIn = plugin.FacebookAgent.prototype.isLoggedIn;
+plugin.FacebookAgent.prototype.isLoggedIn = function(callback){
+    if(this._isLoggedIn())
+        callback(0, "logged in");
+    else
+        callback(1, "logged out")
+};
+
+plugin.FacebookAgent.prototype.requestAccessToken = function(callback){
+    callback(0, this.getAccessToken());
+};
