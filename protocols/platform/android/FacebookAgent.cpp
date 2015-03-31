9137dae1e11e4e226a22d4cc32bf605d236427d1
pandamicro <lphuabin@gmail.com>
2015-03-31 22:02:26 +0800

    Update versions for FacebookAgent

--- protocols/platform/android/FacebookAgent.cpp
+++ protocols/platform/android/FacebookAgent.cpp
@@ -38,9 +38,9 @@ JNIEXPORT void JNICALL Java_org_cocos2dx_plugin_UserFacebook_nativeRequestCallba
 }
 
 static FacebookAgent* s_sharedFacebookAgent = nullptr;
-static const char*  s_cocos2dxVersion = "3.18.0/cocos2d-x-3.4";
-static const char*  s_cocos2dxLuaVersion = "3.18.0/cocos2d-x-lua-3.4";
-static const char*  s_cocos2dxJsVersion  = "3.18.0/cocos2d-js-3.3";
+static const char*  s_cocos2dxVersion = "3.18.0/cocos2d-x-3.5";
+static const char*  s_cocos2dxLuaVersion = "3.18.0/cocos2d-x-lua-3.5";
+static const char*  s_cocos2dxJsVersion  = "3.18.0/cocos2d-js-3.5";
 
 FacebookAgent* FacebookAgent::getInstance()
 {
